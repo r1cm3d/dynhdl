c89b80ec4a713b57b8bcb3b1ab0f77f0834b561e
r1cm3d <r1cm3d@protonmail.com>
2022-04-23 18:28:45 -0300

    Add log and simplelogger libraries
    Those libraries are responsible to log information in stdout and stderr.
    
    Closes #3
    
    See also:
    - https://github.com/rust-lang/log;
    - https://github.com/borntyping/rust-simple_logger;
    
    Signed-off-by: r1cm3d <r1cm3d@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![allow(unused)]
 
+use std::process::exit;
 use clap::Parser;
 use serde_json::{Result, Value};
+use log::{error, info};
+use simple_logger::SimpleLogger;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -11,13 +14,27 @@ struct Cli {
 
     #[clap(short, long)]
     item: String,
+
+    #[clap(long)]
+    pk: String,
 }
 
 fn main() {
+    SimpleLogger::new().init().unwrap();
     let cli = Cli::parse();
+
     let table = cli.table;
     let item = cli.item;
-    let v: Value = serde_json::from_str(&item).expect("Item is not a valid JSON.");
+    let pk = cli.pk;
+    info!("Arguments have successfully parsed into Table ({}), Partition \
+    Key ({}) and Item ({})", table, pk, item);
 
-    println!("Item: {:?}, Numeric: {:?}", v["item"], v["numeric_item"]);
+    info!("Parsing Item ({}) JSON", item);
+    let parse_res = serde_json::from_str(&item);
+    if parse_res.is_err() {
+        error!("Not possible to parse item. Error: {}.", parse_res.unwrap_err());
+        exit(1);
+    }
+    let item: Value = parse_res.unwrap();
+    info!("Item JSON parsed into ({})", item);
 }
