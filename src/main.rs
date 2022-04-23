c62658fc204d9d143efdf5ade2a0ad585de7fa96
r1cm3d <r1cm3d@protonmail.com>
2022-04-23 19:08:30 -0300

    Add AWS Config and AWS DynamoDB dependencies
    This patch adds those dependencies and an example of how to retrieve all
    tables in the AWS Acccount.
    
    See also:
    -
    https://docs.aws.amazon.com/sdk-for-rust/latest/dg/getting-started.html#getting-started-prerequisites;
    - https://crates.io/crates/aws-config;
    - https://crates.io/crates/aws-sdk-dynamodb;
    
    Signed-off-by: r1cm3d <r1cm3d@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use clap::Parser;
 use serde_json::{Result, Value};
 use log::{error, info};
 use simple_logger::SimpleLogger;
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::{Client, Error};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -19,7 +21,10 @@ struct Cli {
     pk: String,
 }
 
-fn main() {
+const DEFAULT_REGION:&str = "sa-east-1";
+
+#[tokio::main]
+async fn main() {
     SimpleLogger::new().init().unwrap();
     let cli = Cli::parse();
 
@@ -37,4 +42,21 @@ fn main() {
     }
     let item: Value = parse_res.unwrap();
     info!("Item JSON parsed into ({})", item);
+
+    info!("Preparing to list all tables");
+    let region_provider = RegionProviderChain::default_provider()
+        .or_else(DEFAULT_REGION);
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+
+    let resp_res = client.list_tables().send().await;
+    if resp_res.is_err() {
+        error!("Not possible to retrieve tables. Error {}.", resp_res.unwrap_err());
+        exit(1)
+    }
+    let list_tables= resp_res.unwrap();
+    let names = list_tables.table_names().unwrap_or_default();
+
+    names.iter()
+        .for_each(|t| info!("Table named ({}) retrieved.", t));
 }
