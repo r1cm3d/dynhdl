077c50117caa54a283fca0f84a06200750ef004e
r1cm3d <r1cm3d@protonmail.com>
2022-04-23 15:17:21 -0300

    Add clap dependency
    This library is a parser to handle CLI arguments easily.
    
    See also:
    - https://github.com/clap-rs/clap
    
    Signed-off-by: r1cm3d <r1cm3d@protonmail.com>

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
